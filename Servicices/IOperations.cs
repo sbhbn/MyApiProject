8f4ed02e2df0e8853cc0a5018bb5e60c5925616a
sbhbn <xiemengy9@gmail.com>
2026-01-31 14:27:06 +0800

    Add files via upload

---
+++ Servicices/IOperations.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MyApiProject.Services
+{
+    public interface IOperations<T, K>
+    {
+        List<T> FindAll();
+        T FindById(K id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        // 假設這是一個條件查詢
+        // 其他方法的定義
+        List<T> FindByCondition(string condition);
+    }
+}
