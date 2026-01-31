16d2d3d8969f443addbadbc4f4b62f622437d295
sbhbn <xiemengy9@gmail.com>
2026-01-31 14:25:43 +0800

    Add files via upload

---
+++ Pages/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApiProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = "歡迎使用 Razor Pages!";
+        }
+    }
+}
